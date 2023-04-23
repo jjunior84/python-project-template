96163c4171f44efbc9343d2effb6b70ab966fb2b
juniojon <jonatas.junior@adidas.com>
2023-04-23 15:07:43 +0100

    documentation

---
+++ python_project_template/dummy.py
@@ -0,0 +1,6 @@
+"""Dummy Module."""
+
+
+def print_dummy() -> None:
+    "Dummy function."
+    print("Python Project Template - Dummy Function")
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,9 @@
 """Python Project Dummy Test Module."""
+from python_project_template.dummy import print_dummy
 
 
 def test_dummy() -> None:
     "Dummy test."
+    print_dummy()
+
     assert True
